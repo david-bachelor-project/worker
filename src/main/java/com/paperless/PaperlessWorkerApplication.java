5f64b77d88415791fce553a311dd84842c430250
DavidJohannesS <davidjohannesSD@gmail.com>
2025-10-16 21:35:30 +0200

    init

---
+++ src/main/java/com/paperless/PaperlessWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.paperless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperlessWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperlessWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paperless/PaperlessWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paperless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
